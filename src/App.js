a47f2f105843c4ef26303ae5227a2eeaac5cd41f
Goro Wen <gorowen56@gmail.com>
2022-09-13 09:39:30 +0800

    Adding particles to the background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
+
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 import './App.css';
 
-class App extends Component {
-    render() {
-        return(
-            <div>
-              <Navigation />
-              <Logo />
-              <Rank />
-              <ImageLinkForm />
-              {/*<FaceRecognition />*/}
-            </div>
-        )
-    }
+const particlesOptions = {
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push",
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
 }
 
+const App = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <div>
+            <Particles
+                className="particles"
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={particlesOptions}
+            />
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForm />
+            {/*<FaceRecognition />*/}
+        </div>
+        
+    );
+};
+
 export default App;
+
